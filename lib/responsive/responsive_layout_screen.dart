55efd7d2b54511bb61bbf948b4fe0f183b596481
Brandon <brandon.thio@yahoo.com.sg>
2022-05-19 10:36:48 +0800

    Add responsive layout

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:verum_flutter/utils/dimensions.dart';
+
+class ResposiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+
+  const ResposiveLayout({
+    Key? key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenWidth) {
+        return webScreenLayout;
+      } else {
+        return mobileScreenLayout;
+      }
+    });
+  }
+}
